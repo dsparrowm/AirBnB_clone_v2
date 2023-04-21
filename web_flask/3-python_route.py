255fe3f72c3421e254717544a08f332fb9ccb183
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-04-21 13:57:22 -0700

    added task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""flask app that runs when called"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_lang(text="is cool"):
+    new_word = text.replace('_', ' ')
+    return "Python {}".format(new_word)
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def prog(text):
+    new_text = text.replace('_', ' ')
+    return "C {}".format(new_text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
