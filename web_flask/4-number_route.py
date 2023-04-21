00c14f81633e7ac8d18d8af150e7ff275d287daa
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-04-21 14:41:01 -0700

    added task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -18,7 +18,7 @@ def python_lang(text="is cool"):
     return "Python {}".format(new_word)
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return "{} is a number".format(n)
 
